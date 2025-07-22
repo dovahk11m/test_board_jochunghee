d29d937f2b92ef184f81bf7c0f70f6242928c9d1
dovahk11m <choongechobiz@gmail.com>
2025-07-22 09:39:47 +0900

    7.22/퍼스트커밋

---
+++ src/main/java/com/chunghee/testboard/TestBoardJochungheeApplication.java
@@ -0,0 +1,13 @@
+package com.chunghee.testboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBoardJochungheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBoardJochungheeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chunghee/testboard/TestBoardJochungheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunghee.testboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBoardJochungheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
